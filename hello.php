aa862e589a3a6994839e86afd4e11638db82c076
czortez <czortez@gmail.com>
2017-12-10 10:17:23 +0100

    Przyklady stringow

--- hello.php
+++ hello.php
@@ -1,6 +1,8 @@
-<?php
-#to jest komentarz
-echo "Hello World";
-?>
-
-To jest czesc poza php§
+<?php 
+echo 'Hello, world!';
+echo 'Oto tekst
+zlozony z kilku linii.';
+echo 'Dodajemy \ 'apostrof\' do stringa. ';
+echo 'Znak niwej linii to \n' ;
+echo 'Zmienna to $zmienna' ;
+?> 
