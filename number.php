db6eeae8b74ab0884e0514f94cd93ddbdb8cc585
czortez <czortez@gmail.com>
2017-12-10 13:02:56 +0100

    cwiczenie 4

---
+++ number.php
@@ -0,0 +1,9 @@
+<?php
+
+$x =44;
+echo $x>9?' Liczba ': ' Cyfra ' ,$x,' jest ', $x%2==0?'parzysta':'nieparzysta',PHP_EOL;
+
+$x =3;
+echo $x>9?' Liczba ': ' Cyfra ' ,$x,' jest ', $x%2==0?'parzysta':'nieparzysta',PHP_EOL;
+
+
