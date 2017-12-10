249af689991174ff9654da87b99e6f290df2b26c
czortez <czortez@gmail.com>
2017-12-10 11:38:43 +0100

    Dopisanie dokumentacji do kazdej zmiennej

--- assign.php
+++ assign.php
@@ -1,19 +1,22 @@
 <?php
-/** Imię psa lub kota.
+/** 
+ * Imię psa lub kota.
  * 
  *  @var string
  */
 $name =  'Bobik';
-/** Liczba Pokoi.
+/** 
+ * Liczba Pokoi.
  * 
- *  @var integer
+ *  @var int
  */
 $roomsCounter = 13;
-/**Aktualna Nazwa zespołu.
+/** 
+ * Aktualna Nazwa zespołu.
  * 
  *  @var string
  */
 $currentBandName = $name;
 echo $name;
 echo $roomsCounter;
-echo $currentBandName;
+echo $currentBandName; 
