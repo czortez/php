97cefd0aa62c5396462fcd6877c5f43538be3a7a
czortez <czortez@gmail.com>
2017-12-10 10:27:22 +0100

    Stringi double quoted

--- hello.php
+++ hello.php
@@ -1,8 +1,7 @@
 <?php 
-echo 'Hello, world!';
-echo 'Oto tekst
-zlozony z kilku linii.';
-echo 'Dodajemy \ 'apostrof\' do stringa. ';
-echo 'Znak niwej linii to \n' ;
-echo 'Zmienna to $zmienna' ;
-?> 
+echo "Hello, world!";
+echo "Oto tekst
+zlozony z kilku linii.";
+echo "Dodajemy \'apostrof\' do stringa. " ;
+echo "Znak niwej linii to \n" ;
+echo "Zmienna to $zmienna" ;
