edb682c0e271ded9aa94b407983179d3d97820d4
czortez <czortez@gmail.com>
2017-12-10 11:36:14 +0100

    Dopisanie dokmentacji do kazdej zmiennej

--- assign.php
+++ assign.php
@@ -1,6 +1,18 @@
 <?php
+/** Imię psa lub kota.
+ * 
+ *  @var string
+ */
 $name =  'Bobik';
+/** Liczba Pokoi.
+ * 
+ *  @var integer
+ */
 $roomsCounter = 13;
+/**Aktualna Nazwa zespołu.
+ * 
+ *  @var string
+ */
 $currentBandName = $name;
 echo $name;
 echo $roomsCounter;
