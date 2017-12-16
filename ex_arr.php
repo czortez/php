cd4fe321a2048aa3d30ac89be94e78b209aa5ddc
czortez <czortez@gmail.com>
2017-12-16 10:14:59 +0100

    Czwarty sposob tablic

--- ex_arr.php
+++ ex_arr.php
@@ -1,3 +1,16 @@
 <?php
-$exArr3 ['klucz'] = 'wartosc';
-var_export ($exArr3);
+$exArr1 = [
+   'klucz' => 'wartosc',
+];
+var_export($exArr1);
+
+$exArr2 = [
+   'wartosc',
+];
+var_export($exArr2);
+
+$exArr3['klucz'] = 'wartosc';
+var_export($exArr3);
+
+$exArr4[] = 'wartosc';
+var_export($exArr4);
