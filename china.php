f234d034903aef797cd9a917cd8d9f00763beea8
czortez <czortez@gmail.com>
2017-12-16 13:34:50 +0100

    Chiński Kalendarz

---
+++ china.php
@@ -0,0 +1,51 @@
+<?php
+$rok = 1984;
+
+
+switch ($rok % 12) {
+    case 0: 
+        echo ' Rok Małpy';
+        break;
+    case 1: 
+        echo ' Rok Koguta';
+        break;
+    case 2: 
+        echo ' Rok Psa';
+        break;
+
+    case 3: 
+        echo ' Rok Świni';
+        break;
+
+    case 4: 
+        echo ' Rok Szczura';
+        break;
+
+    case 5: 
+        echo ' Rok Bawoła';
+        break;
+
+    case 6: 
+        echo ' Rok Tygrysa';
+        break;
+
+    case 7: 
+        echo ' Rok Królika';
+        break;
+
+    case 8: 
+        echo ' Rok Smoka';
+        break;
+
+    case 9: 
+        echo ' Rok Węza';
+        break;
+
+    case 10: 
+        echo ' Rok Konia';
+        break;
+
+    case 11: 
+        echo ' Rok Owcy';
+        break;
+}
