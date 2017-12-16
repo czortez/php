8fbc08343908e01b048a5bbd597297e0da6822d9
czortez <czortez@gmail.com>
2017-12-16 10:19:16 +0100

    6 sposobów wartosci tablic

--- ex_arr.php
+++ ex_arr.php
@@ -9,8 +9,17 @@ $exArr2 = [
 ];
 var_export($exArr2);
 
+$exArr3 = [];
 $exArr3['klucz'] = 'wartosc';
 var_export($exArr3);
 
+$exArr4 = [];
 $exArr4[] = 'wartosc';
 var_export($exArr4);
+
+$exArr5 = [];
+$exArr5{0} = 'wartosc';
+var_export($exArr5);
+
+$exArr6{'klucz'} = 'wartosc';
+var_export($exArr6);
