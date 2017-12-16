e7756e4aa39d4e9fa6c8810ce0434f8a8c51c38d
czortez <czortez@gmail.com>
2017-12-16 10:39:22 +0100

    Tablice jednowymiarowe

---
+++ weekdays.php
@@ -0,0 +1,11 @@
+<?php
+$weekdays = [
+    'monday' => 'poniedziałek',
+    'tuesday' => 'wtorek',
+    'wednesday' => 'sroda',
+    'thursday' => 'czwartek',
+    'friday' => 'piatek',
+    'saturday' => 'sobota',
+    'sunday' => 'niedziela',
+];
+var_export($weekdays);
