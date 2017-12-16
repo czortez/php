9ab6df37d1d322da708370dc3c2741e75bb07db5
czortez <czortez@gmail.com>
2017-12-16 10:30:32 +0100

    Odejmowanie kluczy z tablic

--- ex_arr.php
+++ ex_arr.php
@@ -21,7 +21,24 @@ $exArr5 = [];
 $exArr5{0} = 'wartosc';
 var_export($exArr5);
 
+$exArr6 = [];
 $exArr6{'klucz'} = 'wartosc';
 var_export($exArr6);
 
+$wartosc = $exArr1['klucz'];
+var_export($wartosc);
+$wartosc = $exArr2[0];
+var_export($wartosc);
+$wartosc = $exArr3{'klucz'};
+var_export($wartosc);
+$wartosc = $exArr4{0};
+var_export($wartosc);
 
+unset($exArr1['klucz']);
+var_export($exArr1);
+unset($exArr2[0]);
+var_export($exArr2);
+unset($exArr3{'klucz'});
+var_export($exArr3);
+unset($exArr4{0});
+var_export($exArr4);
