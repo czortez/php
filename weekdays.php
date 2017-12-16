6f1c06b0d6ab0ebab08760191d0c3fd87f0280a2
czortez <czortez@gmail.com>
2017-12-16 11:52:33 +0100

    Dodanie zmiennej  i sprawdzenie jej Ternariuszem

--- weekdays.php
+++ weekdays.php
@@ -1,11 +1,26 @@
 <?php
 $weekdays = [
-    'monday' => 'poniedziałek',
-    'tuesday' => 'wtorek',
-    'wednesday' => 'sroda',
-    'thursday' => 'czwartek',
-    'friday' => 'piatek',
-    'saturday' => 'sobota',
-    'sunday' => 'niedziela',
+    'pl' => [
+        'monday' => 'poniedziałek',
+        'tuesday' => 'wtorek',
+        'wednesday' => 'sroda',
+        'thursday' => 'czwartek',
+        'friday' => 'piatek',
+        'saturday' => 'sobota',
+        'sunday' => 'niedziela',
+    ],
+    'de' => [
+        'monday' => 'montag',
+        'tuesday' => 'dienstag',
+        'wednesday' => 'mittwoch',
+        'thursday' => 'donnerstag',
+        'friday' => 'freitag',
+        'saturday' => 'samstag',
+        'sunday' => 'sonntag',
+    ],
+
 ];
-var_export($weekdays);
+$checkWeekday = 'monday';
+echo isset ($weekdays['pl'][$checkWeekday]) ? 'jest' : 'brak';
+echo isset ($weekdays['en'][$checkWeekday]) ? 'jest' : 'brak';
+echo isset ($weekdays['it'][$checkWeekday]) ? 'jest' : 'brak';
