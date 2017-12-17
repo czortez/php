b309ccf52f857ac8b69b04174db192fe098dced5
czortez <czortez@gmail.com>
2017-12-17 12:07:59 +0100

    Przykładowe dodanie dokumentacji

--- own_fun.php
+++ own_fun.php
@@ -1,7 +1,13 @@
 <?php   
 declare(strict_types=1);
 
-function printFamilyName ($familyName)
+/**
+ * Wyświetlanie z formatowaniem podanego nazwiska.
+ * 
+ * @param string $familyName nazwisko
+ */
+
+function printFamilyName ($familyName) : void   
     {
         echo "Name: $familyName", PHP_EOL;
     }
