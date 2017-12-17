250e02c5806a2361146b0ca8c2a261f8660ef434
czortez <czortez@gmail.com>
2017-12-17 10:21:53 +0100

    cons

---
+++ const.php
@@ -0,0 +1,11 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
+
+const MAX_SLIDES_NUMBER = 99;
+if (!define("MAX_SLIDES_NUMBER",666){
+    define
+
+};
+echo "Maksymalna liczba slajdów: ", MAX_SLIDES_NUMBER, PHP_EOL;
