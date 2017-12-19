4b1a5c6f73c7398b410895f9e53ac4d8656cd395
czortez <czortez@gmail.com>
2017-12-19 22:23:31 +0100

    Petla_foreach__liczby_od_1_do_10

--- foreach_numbers.php
+++ foreach_numbers.php
@@ -2,8 +2,8 @@
 declare(strict_types=1);
 
 $numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    foreach ($numbers as $liczby) {
-        var_export($liczby);
+foreach ($numbers as $liczby) {
+    var_export($liczby);
     
     }
     
