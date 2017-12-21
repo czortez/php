30f64881e2041a44ba6ab5d5b3e0fbb72adcbff8
czortez <czortez@gmail.com>
2017-12-21 07:33:11 +0100

    Petla_for_i_if_-_opisowy_wynik

--- even-odd.php
+++ even-odd.php
@@ -1,9 +1,9 @@
 <?php
-declare(strict_types=1);
-for ($a=1; $a <= 10; $a++) {
-	if($a % 2 === 0){
-        echo $a.' Jest liczbą parzystą ';
-	}else{
-		echo $a.' Jest liczbą nieparzystą ';
-	}
-	}
+declare (strict_types = 1);
+for ($a = 1; $a <= 10; $a++) {
+    if ($a % 2 === 0) {
+        echo $a . ' Jest liczbą parzystą ';
+    } else {
+        echo $a . ' Jest liczbą nieparzystą ';
+    }
+}	                        
