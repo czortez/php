543231893f28ca84e4a8900220e5be40ab3c6b2c
czortez <czortez@gmail.com>
2018-01-13 09:06:54 +0100

    cos

---
+++ more_than_five.php
@@ -0,0 +1,15 @@
+<?php
+declare (strict_types = 1);
+for ($a = 1; $a <= 10; $a++) {
+    if ($a % 2 === 0) {
+        echo $a . ' Jest liczbą parzystą ';
+    } 
+    if ()
+    {
+        continue;
+    }
+
+    else {
+        echo $a . ' Jest liczbą nieparzystą ';
+    }
+}	                        
